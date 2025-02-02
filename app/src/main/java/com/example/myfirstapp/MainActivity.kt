dfd6d84e6701f3db1d029da2b72bacbab89f4058
Divesh Aggarwal <diveshagarwal213@gmail.com>
2025-02-02 22:58:13 +0530

    unitConverter

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,22 +8,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myfirstapp.ui.theme.MyFirstAppTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,10 +42,6 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyFirstAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
                     UnitConverter(modifier = Modifier.padding(innerPadding))
                 }
             }
@@ -45,45 +50,142 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun UnitConverter(modifier: Modifier = Modifier){
-    Column (modifier=modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Unit Converter", modifier=Modifier.padding(vertical = 16.dp))
-        OutlinedTextField(value = "", onValueChange = {})
+fun UnitConverter(modifier: Modifier = Modifier) {
+    //states
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Meters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember { mutableDoubleStateOf(1.00) }
+    val oConversionFactor = remember { mutableDoubleStateOf(1.00) }
+
+    // handlers
+    fun convertUnits() {
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        outputValue =
+            (((inputValueDouble * conversionFactor.doubleValue * 100.0 / oConversionFactor.doubleValue)).roundToInt() / 100.0).toString()
+    }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Unit Converter",
+            modifier = Modifier.padding(vertical = 16.dp),
+            style = MaterialTheme.typography.headlineLarge
+        )
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+                inputValue = it
+                convertUnits()
+            },
+            label = { Text("Enter  value") },
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
         Row {
             Box {
-                val context = LocalContext.current
-                Button(onClick = {Toast.makeText(context,"Clicked!",Toast.LENGTH_LONG).show()}) {
-                    Text("Select")
-                    Icon(Icons.Default.ArrowDropDown,"Arrow Down")
+                Button(onClick = {
+                    iExpanded = true
+                }) {
+                    Text(inputUnit)
+                    Icon(Icons.Default.ArrowDropDown, "Arrow Down")
+                }
+                DropdownMenu(expanded = iExpanded, onDismissRequest = {
+                    iExpanded = false
+                }) {
+                    DropdownMenuItem(text = {
+                        Text("Centimeters")
+                    }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Centimeters"
+                        conversionFactor.doubleValue = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {
+                        Text("Meters")
+                    }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Meters"
+                        conversionFactor.doubleValue = 1.0
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {
+                        Text("Feet")
+                    }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Feet"
+                        conversionFactor.doubleValue = 0.3048
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {
+                        Text("Millimeters")
+                    }, onClick = {
+                        iExpanded = false
+                        inputUnit = "Millimeters"
+                        conversionFactor.doubleValue = 0.001
+                        convertUnits()
+                    })
                 }
             }
+            Spacer(modifier = Modifier.width(16.dp))
             Box {
-                val context = LocalContext.current
-                Button(onClick = {Toast.makeText(context,"Clicked!",Toast.LENGTH_LONG).show()}) {
-                    Text("Select")
-                    Icon(Icons.Default.ArrowDropDown,"Arrow Down")
+                Button(onClick = {
+                    oExpanded = true
+                }) {
+                    Text(outputUnit)
+                    Icon(Icons.Default.ArrowDropDown, "Arrow Down")
+                }
+                DropdownMenu(expanded = oExpanded, onDismissRequest = {
+                    oExpanded = false
+                }) {
+                    DropdownMenuItem(text = {
+                        Text("Centimeters")
+                    }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Centimeters"
+                        oConversionFactor.doubleValue = 0.01
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {
+                        Text("Meters")
+                    }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Meters"
+                        oConversionFactor.doubleValue = 1.0
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {
+                        Text("Feet")
+                    }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Feet"
+                        oConversionFactor.doubleValue = 0.3048
+                        convertUnits()
+                    })
+                    DropdownMenuItem(text = {
+                        Text("Millimeters")
+                    }, onClick = {
+                        oExpanded = false
+                        outputUnit = "Millimeters"
+                        oConversionFactor.doubleValue = 0.001
+                        convertUnits()
+                    })
                 }
             }
+
         }
-        Text(text = "Result:")
+        Text(
+            text = "Result: $outputValue $outputUnit",
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    MyFirstAppTheme {
-//
-//    }
-//}
 
 @Preview(showBackground = true)
 @Composable
